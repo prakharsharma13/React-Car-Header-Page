e5054ce2c0a3d274078c7dd75b3d3a623ab31323
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-09-05 23:55:25 +0530

    added animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import React, { useState } from 'react'
-import Background from './components/Background/Background';
-import Navbar from './components/Navbar/Navbar';
-import Hero from './components/Hero/Hero';
+import React, { useEffect, useState } from "react";
+import Background from "./components/Background/Background";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
   let heroData = [
-    {text1: "Dive into", text2: "what you love"},
-    {text1: "Indulge", text2: "your passions"},
-    {text1: "Give into", text2: "your passions"},
-  ]
+    { text1: "Dive into", text2: "what you love" },
+    { text1: "Indulge", text2: "your passions" },
+    { text1: "Give into", text2: "your passions" },
+  ];
 
-  const[heroCount, setHeroCount] = useState(2);
-  const[playStatus, setPlayStatus] = useState(false);
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setHeroCount((count) => {
+        return count === 2 ? 0 : count + 1;
+      });
+    }, 3000);
+  },[]);
   return (
     <div>
       <Background playStatus={playStatus} heroCount={heroCount} />
@@ -23,7 +31,7 @@ const App = () => {
         playStatus={playStatus}
       />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
